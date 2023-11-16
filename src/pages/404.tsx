5b375af4bf9f100a8664327b71a1fadc018f93e6
Leda Wolf <leda@hey.com>
2023-11-16 17:17:13 +0100

    Add changes to config

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -38,7 +38,7 @@ const NotFoundPage: React.FC<PageProps> = () => {
           </>
         ) : null}
         <br />
-        <Link to="/">Go home</Link>.
+        <Link to="/">Nevermind.</Link>.
       </p>
     </main>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,33 @@
-import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
-import {Page, HeroSection, AboutSection, ContactSection} from "gatsby-theme-portfolio-minimal"
+import * as React from "react";
+import type { HeadFC, PageProps } from "gatsby";
+import {
+  Page,
+  HeroSection,
+  AboutSection,
+  ContactSection,
+  Seo,
+  Section,
+  Animation,
+} from "gatsby-theme-portfolio-minimal";
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Page useSplashScreenAnimation>
+      <Seo title="Leda Wolf" />
       <HeroSection sectionId="hero" />
-      <AboutSection sectionId="about" heading="About" />
+      <AboutSection sectionId="about" heading="Who?" />
+      <Animation>
+        <Section sectionId="testimonials" heading="Testimonials">
+          <div>
+            <span>TBD</span>
+          </div>
+        </Section>
+      </Animation>
       <ContactSection sectionId="github" heading="What is she up to?" />
     </Page>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Home Page</title>;
