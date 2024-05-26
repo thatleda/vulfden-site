775e9f7a0b693b40b4161413e6f492b5b85245fa
Leda Wolf <leda@hey.com>
2024-05-26 14:45:30 +0200

    Extend tests

--- src/components/base/Pagination.spec.tsx
+++ src/components/base/Pagination.spec.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { expect, it } from "vitest";
 
-import Pagination from "./Pagination";
+import Pagination from "components/base/Pagination";
 
 import { render, screen } from "@testing-library/react";
 
@@ -15,10 +15,28 @@ it("should not show a component if totalPages = 1", () => {
     />
   );
 
-  expect(screen.queryAllByRole('link')).toHaveLength(0)
+  expect(screen.queryAllByRole("link")).toHaveLength(0);
 });
 
-it("should have disabled previous page if no previous page available", () => {
+it("should link to the correct pages from the last one", () => {
+  render(
+    <Pagination
+      currentPage={2}
+      hasNextPage={false}
+      hasPreviousPage={true}
+      totalPages={2}
+    />
+  );
+
+  expect(
+    screen.getByLabelText("Previous page").getAttribute("href")
+  ).toStrictEqual("/ramblings");
+  expect(
+    screen.getByLabelText("Current page, page 2").getAttribute("href")
+  ).toStrictEqual("/ramblings/2");
+});
+
+it("should link to the correct pages from the first one", () => {
   render(
     <Pagination
       currentPage={1}
@@ -28,5 +46,56 @@ it("should have disabled previous page if no previous page available", () => {
     />
   );
 
-  screen.debug()
+  expect(screen.getByLabelText("Next page").getAttribute("href")).toStrictEqual(
+    "/ramblings/2"
+  );
+  expect(
+    screen.getByLabelText("Current page, page 1").getAttribute("href")
+  ).toStrictEqual("/ramblings");
+});
+
+it("should show the current page as selected but not disabled", () => {
+  render(
+    <Pagination
+      currentPage={2}
+      hasNextPage={false}
+      hasPreviousPage={true}
+      totalPages={2}
+    />
+  );
+
+  const currentPageLink = screen.getByLabelText("Current page, page 2");
+
+  expect(currentPageLink.getAttribute("selected")).toBeDefined();
+  expect(currentPageLink.getAttribute("disabled")).toBeNull();
+});
+
+it("should disable previous page if no previous page available", () => {
+  render(
+    <Pagination
+      currentPage={1}
+      hasNextPage={true}
+      hasPreviousPage={false}
+      totalPages={2}
+    />
+  );
+
+  expect(
+    screen.getByLabelText("Previous page").getAttribute("disabled")
+  ).toBeDefined();
+});
+
+it("should disable next page if no next page available", () => {
+  render(
+    <Pagination
+      currentPage={1}
+      hasNextPage={false}
+      hasPreviousPage={true}
+      totalPages={2}
+    />
+  );
+
+  expect(
+    screen.getByLabelText("Next page").getAttribute("disabled")
+  ).toBeDefined();
 });
